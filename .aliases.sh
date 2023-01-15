97b64cf55294d9681596922b10239d7dac0fb6e0
jit <serridd@gmail.com>
2023-01-15 20:14:11 +0300

    Cleanup aliases.

--- .aliases.sh
+++ .aliases.sh
@@ -8,7 +8,6 @@ alias l="ls"
 alias ll='ls -oh'
 alias la='ls -A'
 alias v="vim"
-alias e="emacs"
 alias j="jobs"
 alias c="cdl"
 alias o="xdg-open"
@@ -17,19 +16,9 @@ alias pi="p -i"
 alias nf="touch"
 alias hexdump="hexdump -C"
 alias extract="tar -xvf"
-alias where="readlink -f"
+alias absolute="readlink -f"
 alias pong="ping 8.8.8.8"
-alias update="sudo pacman -Syu"
-alias show="clear;tree"
-alias new="urxvt &"
-alias summ="du -h --summarize *"
-alias rmsc="rm -f Screenshot*"
 alias Find="find 2>/dev/null"
-alias sgpm="sudo systemctl start gpm"
-
-alias cag="clear; cargo"
-
-alias chelp="clang --help | grep"
 
 function nixgc() {
     # Delete old system generations
