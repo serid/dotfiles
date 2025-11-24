308c21ae58229ad2be85b4f75e2ca8db59944139
jit <serridd@gmail.com>
2025-11-24 21:05:53 +0300

    Add a Comma.

--- flake.nix
+++ flake.nix
@@ -2,15 +2,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     impermanence.url = "github:nix-community/impermanence";
+
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, impermanence, ... }@inputs: {
+  outputs = { self, nixpkgs, impermanence, nix-index-database, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
         ./hardware-configuration.nix
         ./configuration.nix
 
         impermanence.nixosModules.default
+
+        nix-index-database.nixosModules.nix-index
+        { programs.nix-index-database.comma.enable = true; }
 
         # Graphical docker WIP
         (nixpkgs.lib.attrsets.optionalAttrs false {
