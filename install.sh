830a1faed71eb0264f20737f6d576ec454a80809
jit <serridd@gmail.com>
2025-11-25 04:34:37 +0300

    Home manager, yuppie!!

--- install.sh
+++ install.sh
@@ -1,9 +1,1 @@
-ln -sf /workshop/dotfiles/flake.nix /etc/nixos/flake.nix
-
-# TODO: home-manager
-ln -sf /workshop/dotfiles/.aliases.sh ~/.aliases.sh
-ln -sf /workshop/dotfiles/.vimrc ~/.vimrc
-ln -sf /workshop/dotfiles/settings.json ~/.config/Code/User/settings.json
-ln -sf /workshop/dotfiles/keybindings.json ~/.config/Code/User/keybindings.json
-ln -sf /workshop/dotfiles/config.toml ~/.config/helix/config.toml
-ln -sf /workshop/dotfiles/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+ln -s /workshop/dotfiles/flake.nix /etc/nixos/flake.nix
